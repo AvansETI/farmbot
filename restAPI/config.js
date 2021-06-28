d21d82d4ce84508dc890326ec7f71576a2ad5a4f
Lars 2 <l15vdef@gmail.com>
2021-06-28 10:26:27 +0200

    [Fix] Env variables

--- restAPI/config.js
+++ restAPI/config.js
@@ -1,22 +1,22 @@
 export default {
-  user: {
-    email: "******",
-    password: "******",
-  },
-  http: {
-    port: 3000,
-  },
-  api: {
-    URL: "https://my.farmbot.io/api",
-  },
-  database: {
-    address: "mongodb://127.0.0.1:27017/farmbot",
-    username: "****",
-    password: "****"
-  },
-  mqttCamera: {
-    broker: "mqtt://*****",
-    username: "****",
-    password: "*****",
-  },
+    user: {
+        email: process.env.MFB_USER || "******",
+        password: process.env.MFB_PASSWORD || "******",
+    },
+    http: {
+        port: process.env.PORT || 3000,
+    },
+    api: {
+        URL: process.env.API_URL || "https://my.farmbot.io/api",
+    },
+    database: {
+        address: process.env.DB_URL || "mongodb://127.0.0.1:27017/farmbot",
+        username: process.env.DB_USER || "****",
+        password: process.env.DB_PASSWORD || "****"
+    },
+    mqttCamera: {
+        broker: process.env.BROKER_URL || "mqtt://*****",
+        username: process.env.BROKER_USER || "****",
+        password: process.env.BROKER_PASSWORD || "*****",
+    },
 };
--- restAPI/main.js
+++ restAPI/main.js
@@ -14,35 +14,35 @@ import config from "./config.js";
 const app = express();
 
 mongoose.connect(config.database.address, {
-  user: config.database.username,
-  pass: config.database.password,
-  authSource: "admin",
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    user: config.database.username,
+    pass: config.database.password,
+    authSource: "admin",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 
 const farmbotManager = new FarmbotManager(
-  config.user.email,
-  config.user.password,
+    config.user.email,
+    config.user.password,
 );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(
-  bodyParser.raw({
-    inflate: true,
-    limit: "15mb",
-    type: "image/*",
-  })
+    bodyParser.raw({
+        inflate: true,
+        limit: "15mb",
+        type: "image/*",
+    })
 );
 
-app.all("*", function (req, res, next) {
-  console.log(
-    `[${new Date().toISOString()}] [${req.method}] ${req.url} has been invoked!`
-  );
-  next();
+app.all("*", function(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] [${req.method}] ${req.url} has been invoked!`
+    );
+    next();
 });
 
 app.use("/image", imageEndpoint);
@@ -51,26 +51,26 @@ app.use(express.static("public"));
 
 await farmbotManager.connect();
 
-cron.schedule("*/60 10-22 * * *", async () => {
-  await farmbotManager.performDataSequence();
+cron.schedule(process.env.DATASEQUENCE_SCHEDULE || "*/60 10-22 * * *", async() => {
+    await farmbotManager.performDataSequence();
 });
 
-cron.schedule("* 8 * * * ", async () => {
-  await farmbotManager.performWaterSequence();
+cron.schedule(process.env.WATERSEQUENCE_SCHEDULE || "* 8 * * * ", async() => {
+    await farmbotManager.performWaterSequence();
 });
 
 app.get("/dataSequence", (req, res) => {
-  farmbotManager.performDataSequence();
-  res.end("Data sequence has started!");
+    farmbotManager.performDataSequence();
+    res.end("Data sequence has started!");
 });
 
 app.get("/waterSequence", (req, res) => {
-  farmbotManager.performWaterSequence();
-  res.end("Water sequence has started!");
+    farmbotManager.performWaterSequence();
+    res.end("Water sequence has started!");
 });
 
-app.listen(config.http.port, function () {
-  console.log(`Server started at port: ${config.http.port} `);
+app.listen(config.http.port, function() {
+    console.log(`Server started at port: ${config.http.port} `);
 });
 
 //farmbotManager.performDataSequence();
