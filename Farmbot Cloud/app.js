1d87c07857e8d4892cb39064b19e9f9cf01171c6
Lars <larsvangisbergen@gmail.com>
2021-12-15 10:49:14 +0100

    webhook new endpoint functionality added

--- Farmbot Cloud/app.js
+++ Farmbot Cloud/app.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
 const Firestore = require('@google-cloud/firestore');
 const { v4: uuidv4 } = require('uuid');
+const express = require("express")
+const bodyParser = require("body-parser")
 
 
 const db = new Firestore({
@@ -7,25 +10,43 @@ const db = new Firestore({
   keyFilename: 'D:/Schooljaar4/Farmbot/farmbotfirebasecredentials.json',
 });
 
+async function writePlant(image_id, plant_name, database){
+  const docRef = db.collection('Plants').doc(uuidv4());
 
-async function writePlant(image_id, plant_name){
-    const docRef = db.collection('Plants').doc(uuidv4());
-
-    await docRef.set({
-        image_id: image_id,
-        plant_name: plant_name,
-    });
+  await docRef.set({
+      image_id: image_id,
+      plant_name: plant_name,
+  });
 }
 
-async function printPlants(){
-    const snapshot = await db.collection('Plants').get();
-    snapshot.forEach((doc) => {
-      console.log(doc.id, '=>', doc.data());
-    });
+async function printPlants(database){
+  const snapshot = await db.collection('Plants').get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());
+  });
 }
 
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+// Tell express to use body-parser's JSON parsing
+app.use(bodyParser.json())
+// Start express on the defined port
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.post("/image", (req, res) => {
+  const photoId = req.query.messageId;
+
+  fs.writeFile(`images/${photoId}.jpg`, req.body, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.status(200).send();
+  });
+});
 
 
+//printPlants(db)
 
-writePlant(8387387, "banoplant")
-printPlants()
+
