e73c6c0ed0306711b61791b8c71e7709023f7e37
Ties Tienhoven <tiestienhoven@gmail.com>
2021-12-21 14:34:38 +0100

    Added some extra logging for mongoDB

--- restAPI/main.js
+++ restAPI/main.js
@@ -15,15 +15,21 @@ import log from "./utils/logger.js"
 const app = express();
 const logSource = "Main"
 
+log(logSource, "Startup", `Trying to connect to mongoDB with ${config.database.username}, ${config.database.password}`)
+
 mongoose.connect(config.database.address, {
     user: config.database.username,
     pass: config.database.password,
     authSource: "admin",
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}).then(
+    log(logSource, "Startup", "Established connection with MongoDB")
+).catch(
+    log(logSource, "Startup Failed", "Connection with MongoDB Failed")
+)
 
-log(logSource, "Startup", "Established connection with MongoDB")
+
 
 const farmbotManager = new FarmbotManager(
     config.user.email,
