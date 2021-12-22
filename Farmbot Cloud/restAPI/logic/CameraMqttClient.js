ddd5c438c4e3bbe1acfd08e75bb4590283105e12
ChiemS <cp.stevens@student.avans.nl>
2021-12-22 12:31:51 +0100

    Working MQTT with google pub/sub

--- Farmbot Cloud/restAPI/logic/CameraMqttClient.js
+++ Farmbot Cloud/restAPI/logic/CameraMqttClient.js
@@ -18,7 +18,7 @@ export default class CameraClient {
     this.client = new PS.PubSub();
     this.subscribteToTopics();
   }
-  
+
 
   /*  subscribteToTopics()
       subscribe to the MQTT broker for the camera and the measurements.
@@ -26,20 +26,24 @@ export default class CameraClient {
   subscribteToTopics() {
     log("CameraClient", "Connection", "Connected");
 
-    // Create an event handler to handle messages
-    let messageCount = 0;
-    const messageHandler = message => {
-      console.log(`Received message ${message.id}:`);
-      console.log(`\tData: ${message.data}`);
-      console.log(`\tAttributes: ${message.attributes}`);
-      messageCount += 1;
-  
-      // "Ack" (acknowledge receipt of) the message
+    const messageHandler = (message) => {
+      let bufferOriginal = Buffer.from(JSON.parse(JSON.stringify(message.data)).data);
+      let json = JSON.parse(bufferOriginal.toString('utf8'));
+      this.onHandleTopics(json.topic, JSON.stringify(json));
       message.ack();
-    };
+    }
 
-    pubSubClient.subscription(`sensor-${this.farmbotId}-camera`).on('message', takePicture);
-    pubSubClient.subscription(`sensor-${this.farmbotId}-measurements`).on('message', receiveMeasurements);
+    this.client.subscription(`sensor-${this.farmbotId}-logs`).on('message', messageHandler).on('error', (error) => {log("CameraClient", "Subscription", "Error Subscribing")});
+    this.client.subscription(`sensor-${this.farmbotId}-measurement`).on('message', messageHandler).on('error', (error) => {log("CameraClient", "Subscription", "Error Subscribing")});
+  }
+
+  /*  onHandleTopics(topic, message)
+      Handle the right message based on the incoming topic.
+  */
+  onHandleTopics(topic, message) {
+    if (this.responseFunction) {
+      this.responseFunction(message);
+    }
   }
 
   /*  takePicture()
@@ -49,14 +53,16 @@ export default class CameraClient {
   takePicture() {
     return new Promise((resolve, reject) => {
       setTimeout(async () => {
+        this._removeResponseFunction();
         reject("Camera did not respond within 20 seconds");
       }, 20000);
 
       this._addResponseFunction((data) => {
+        this._removeResponseFunction();
         resolve(data);
       });
 
-      pubSubClient.topic(`sensor-${this.farmbotId}-camera`).publish("");
+      this.client.topic(`sensor-${this.farmbotId}-camera`).publish(Buffer.from("Take Picture"));
     });
   }
 
@@ -67,14 +73,27 @@ export default class CameraClient {
   receiveMeasurements() {
     return new Promise((resolve, reject) => {
       setTimeout(async () => {
+        this._removeResponseFunction();
         reject("Sensor did not respond within 20 seconds");
       }, 20000);
 
       this._addResponseFunction((data) => {
+        this._removeResponseFunction();
+        console.log(data);
         resolve(data);
       });
 
-      pubSubClient.topic(`sensor-${this.farmbotId}-measurements`).publish("");
+      this.client.topic(`sensor-${this.farmbotId}-measurement`).publish(Buffer.from("Measurements"));
     });
+  }
+
+  _addResponseFunction(responseFunction) {
+    if (!this.responseFunction) {
+      this.responseFunction = responseFunction;
+    }
+  }
+
+  _removeResponseFunction() {
+    this.responseFunction = undefined;
   }
 }
