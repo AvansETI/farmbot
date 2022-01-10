185de0316e2347702af9fb13a9f8d14eb1e98967
Ties Tienhoven <tiestienhoven@gmail.com>
2022-01-10 11:26:15 +0100

    Removed config.js from the gitignore

---
+++ Farmbot Cloud/restAPI/config.js
@@ -0,0 +1,13 @@
+export default {
+    googleSecrets: {
+        email: "FarmbotApiEmail",
+        password: "FarmbotApiPassword",
+        version: 1
+    },
+    http: {
+        port: 3000,
+    },
+    api: {
+        URL: "https://my.farmbot.io/api",
+    },
+};
