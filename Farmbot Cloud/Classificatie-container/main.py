bd27b2049ec148cb6be579f4f471192c5d66ca25
Ties Tienhoven <tiestienhoven@gmail.com>
2022-01-11 15:21:52 +0100

    Created request to pass filepath which gives image url

--- Farmbot Cloud/Classificatie-container/main.py
+++ Farmbot Cloud/Classificatie-container/main.py
@@ -1,11 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, request
+from google.cloud import storage
 
 app = Flask(__name__)
+
+STORAGE_BUCKET_NAME = "farmbot-avans-cloud.appspot.com"
 
 @app.route('/')
 def get_data():
     return "Hello World!"
+
+@app.route('/classify', methods = ['POST'])
+def classify_image():
+    incoming_json = request.get_json()
+    print(incoming_json['filepath'])
+
+    client = storage.Client()
+    bucket = client.bucket(STORAGE_BUCKET_NAME)
+    blob = bucket.blob(incoming_json['filepath'])
+    image_url = blob.public_url
+    
+    return "Good test 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
