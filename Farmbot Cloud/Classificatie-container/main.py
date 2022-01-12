f96002bc597ef03ce64a7e0e76de32b3d7ea2c62
ChiemS <cp.stevens@student.avans.nl>
2022-01-12 10:54:33 +0100

    Container running on cloud

--- Farmbot Cloud/Classificatie-container/main.py
+++ Farmbot Cloud/Classificatie-container/main.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, request
 from google.cloud import storage
+import torch
+import yaml
+import numpy as np
 
 app = Flask(__name__)
+model = None
 
 STORAGE_BUCKET_NAME = "farmbot-avans-cloud.appspot.com"
 
@@ -19,8 +23,16 @@ def classify_image():
     bucket = client.bucket(STORAGE_BUCKET_NAME)
     blob = bucket.blob(incoming_json['filepath'])
     image_url = blob.public_url
+
+    
+    results = model(image_url)
+
+    #img = results.render()
+    boxes = results.xyxyn
+    print(boxes)
     
     return "Good test 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    model = torch.hub.load('yolo', 'custom', path='best.pt', source='local')
