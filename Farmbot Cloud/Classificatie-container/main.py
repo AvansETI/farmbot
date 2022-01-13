e349f5434a23e69d5487e3a51e00af5c0b64534c
ChiemS <cp.stevens@student.avans.nl>
2022-01-13 15:05:31 +0100

    Containerclassification working, TODO upload file and metadata to storage

--- Farmbot Cloud/Classificatie-container/main.py
+++ Farmbot Cloud/Classificatie-container/main.py
@@ -1,3 +1,4 @@
+from email.mime import image
 import os
 from flask import Flask, request
 from google.cloud import storage
@@ -25,15 +26,21 @@ def classify_image():
     client = storage.Client()
     bucket = client.bucket(STORAGE_BUCKET_NAME)
     blob = bucket.blob(incoming_json['filepath'])
+    print("Blob: "+ blob)
     image_url = blob.public_url
+    print(image_url)
 
     classification_model = torch.hub.load(
         'yolo', 'custom', path='best.pt', source='local')
 
     results = classification_model(image_url)
 
-    #img = results.render()
+    # img = results.render()
+    # print(img[0]) <-- image als array?
+    # converteren naar blob en opslaan in cloud storage
+
     boxes = results.xyxyn
+    # boxes ook opslaan in cloud firestore? format boxes --> [x,y,x,y,n] is een tensor
     print(boxes)
 
     return "Good test 🚀"
