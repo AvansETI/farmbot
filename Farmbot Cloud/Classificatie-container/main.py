27d6f520f0523b557fa5553396bdccfed0a4bd2b
ChiemS <cp.stevens@student.avans.nl>
2022-01-13 09:49:35 +0100

    Classificatie model changes

--- Farmbot Cloud/Classificatie-container/main.py
+++ Farmbot Cloud/Classificatie-container/main.py
@@ -3,18 +3,21 @@ from flask import Flask, request
 from google.cloud import storage
 import torch
 import yaml
+import cv2
 import numpy as np
 
 app = Flask(__name__)
-model = None
+classification_model = None
 
 STORAGE_BUCKET_NAME = "farmbot-avans-cloud.appspot.com"
+
 
 @app.route('/')
 def get_data():
     return "Hello World!"
 
-@app.route('/classify', methods = ['POST'])
+
+@app.route('/classify', methods=['POST'])
 def classify_image():
     incoming_json = request.get_json()
     print(incoming_json['filepath'])
@@ -24,15 +27,19 @@ def classify_image():
     blob = bucket.blob(incoming_json['filepath'])
     image_url = blob.public_url
 
-    
-    results = model(image_url)
+    classification_model = torch.hub.load(
+        'yolo', 'custom', path='best.pt', source='local')
+
+    results = classification_model(image_url)
 
     #img = results.render()
     boxes = results.xyxyn
     print(boxes)
-    
+
     return "Good test 🚀"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-    model = torch.hub.load('yolo', 'custom', path='best.pt', source='local')
+    classification_model = torch.hub.load(
+        'yolo', 'custom', path='best.pt', source='local')
